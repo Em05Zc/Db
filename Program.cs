e7c5f308fc3f43ea2e5f4a0c5828f6f1fba55e31
Emmanuel Zacharioudakis <98521501+Em05Zc@users.noreply.github.com>
2023-09-27 10:50:39 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// This is the beginning of C#
+
+namespace Beginning
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("This is my first program");
+            
+            Console.WriteLine("Type in your name");
+            string name = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("No name was entered");
+            }
+            else if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("No name was entered");
+            }
+            else
+            {
+                Console.WriteLine("Name: " + name);
+            }
+        }
+    }
+}
